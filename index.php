1823cd4ae28ceb3c723c18065ccdaa155d4cc838
Grégory LACROIX <gregorylacroix78@gmail.com>
2020-01-07 10:29:32 +0100

    ajout nav + footer

--- index.php
+++ index.php
@@ -5,12 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Mon Site </title>
+    <title>Mon Site</title>
 </head>
 <body>
-<div class="container">
-    <h1 class="display-4 text-center text-info">Découverte GIT / GITHUB</h1>
-    <p class="alert alert-info p-2 text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, dignissimos? Ipsam consequuntur alias rem voluptate nisi unde, commodi facere mollitia recusandae aliquam et accusamus, reprehenderit quidem modi cupiditate, asperiores voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis iusto, vero assumenda quod ratione perferendis, ad nihil sapiente vitae, est doloremque quidem. Voluptatem, quas aut a sapiente consequatur similique delectus!locale_filter_matche Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio deserunt magnam quos ab, ex odio excepturi facere quibusdam sit? Harum aliquid cum laborum, quia eveniet ratione nemo exercitationem expedita eaque!</p>
-</div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Mon Site</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Boutique</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Profil</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+
+    <div class="container">
+        <h1 class="display-4 text-center text-info">Découverte GIT / GITHUB</h1>
+        <p class="alert alert-info p-2 text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, dignissimos? Ipsam consequuntur alias rem voluptate nisi unde, commodi facere mollitia recusandae aliquam et accusamus, reprehenderit quidem modi cupiditate, asperiores voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis iusto, vero assumenda quod ratione perferendis, ad nihil sapiente vitae, est doloremque quidem. Voluptatem, quas aut a sapiente consequatur similique delectus!locale_filter_matche Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio deserunt magnam quos ab, ex odio excepturi facere quibusdam sit? Harum aliquid cum laborum, quia eveniet ratione nemo exercitationem expedita eaque!</p>
+    </div>
+
+    <footer class="fixed-bottom bg-dark text-white text-center p-3">
+        &copy; 2019 - Grégory LACROIX
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>   
